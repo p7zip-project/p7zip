3e0e00c9dea697095a07e3ec8fcb9e12b9f225a7
jinfeihan57 <jinfeihan57@gmail.com>
2021-05-19 13:06:12 +0800

    fix issue 144

--- CPP/7zip/Compress/Lz5Decoder.cpp
+++ CPP/7zip/Compress/Lz5Decoder.cpp
@@ -3,15 +3,16 @@
 #include "StdAfx.h"
 #include "Lz5Decoder.h"
 
-int Lz5Read(void *arg, LZ5MT_Buffer * in)
+int Lz5Read(void *arg, LZ5MT_Buffer *in)
 {
-  struct Lz5Stream *x = (struct Lz5Stream*)arg;
+  struct Lz5Stream *x = (struct Lz5Stream *)arg;
   size_t size = in->size;
 
   HRESULT res = ReadStream(x->inStream, in->buf, &size);
 
   /* catch errors */
-  switch (res) {
+  switch (res)
+  {
   case E_ABORT:
     return -2;
   case E_OUTOFMEMORY:
@@ -28,19 +29,20 @@ int Lz5Read(void *arg, LZ5MT_Buffer * in)
   return 0;
 }
 
-int Lz5Write(void *arg, LZ5MT_Buffer * out)
+int Lz5Write(void *arg, LZ5MT_Buffer *out)
 {
-  struct Lz5Stream *x = (struct Lz5Stream*)arg;
+  struct Lz5Stream *x = (struct Lz5Stream *)arg;
   UInt32 todo = (UInt32)out->size;
   UInt32 done = 0;
 
   while (todo != 0)
   {
     UInt32 block;
-    HRESULT res = x->outStream->Write((char*)out->buf + done, todo, &block);
+    HRESULT res = x->outStream->Write((char *)out->buf + done, todo, &block);
 
     /* catch errors */
-    switch (res) {
+    switch (res)
+    {
     case E_ABORT:
       return -2;
     case E_OUTOFMEMORY:
@@ -67,126 +69,132 @@ int Lz5Write(void *arg, LZ5MT_Buffer * out)
   return 0;
 }
 
-namespace NCompress {
-namespace NLZ5 {
-
-CDecoder::CDecoder():
-  _processedIn(0),
-  _processedOut(0),
-  _inputSize(0),
-  _numThreads(NWindows::NSystem::GetNumberOfProcessors())
+namespace NCompress
 {
-  _props.clear();
-}
+  namespace NLZ5
+  {
 
-CDecoder::~CDecoder()
-{
-}
+    CDecoder::CDecoder() : _processedIn(0),
+                           _processedOut(0),
+                           _inputSize(0),
+                           _numThreads(NWindows::NSystem::GetNumberOfProcessors())
+    {
+      _props.clear();
+    }
 
-STDMETHODIMP CDecoder::SetDecoderProperties2(const Byte * prop, UInt32 size)
-{
-  DProps *pProps = (DProps *)prop;
+    CDecoder::~CDecoder()
+    {
+    }
 
-  switch (size) {
-  case 3:
-    memcpy(&_props, pProps, 3);
-    return S_OK;
-  case 5:
-    memcpy(&_props, pProps, 5);
-    return S_OK;
-  default:
-    return E_NOTIMPL;
-  }
-}
+    STDMETHODIMP CDecoder::SetDecoderProperties2(const Byte *prop, UInt32 size)
+    {
+      DProps *pProps = (DProps *)prop;
 
-STDMETHODIMP CDecoder::SetNumberOfThreads(UInt32 numThreads)
-{
-  const UInt32 kNumThreadsMax = LZ5MT_THREAD_MAX;
-  if (numThreads < 1) numThreads = 1;
-  if (numThreads > kNumThreadsMax) numThreads = kNumThreadsMax;
-  _numThreads = numThreads;
-  return S_OK;
-}
+      switch (size)
+      {
+      case 3:
+        memcpy((void *)&_props, (void *)pProps, 3);
+        return S_OK;
+      case 5:
+        memcpy((void *)&_props, (void *)pProps, 5);
+        return S_OK;
+      default:
+        return E_NOTIMPL;
+      }
+    }
 
-HRESULT CDecoder::SetOutStreamSizeResume(const UInt64 * /*outSize*/)
-{
-  _processedOut = 0;
-  return S_OK;
-}
+    STDMETHODIMP CDecoder::SetNumberOfThreads(UInt32 numThreads)
+    {
+      const UInt32 kNumThreadsMax = LZ5MT_THREAD_MAX;
+      if (numThreads < 1)
+        numThreads = 1;
+      if (numThreads > kNumThreadsMax)
+        numThreads = kNumThreadsMax;
+      _numThreads = numThreads;
+      return S_OK;
+    }
 
-STDMETHODIMP CDecoder::SetOutStreamSize(const UInt64 * outSize)
-{
-  _processedIn = 0;
-  RINOK(SetOutStreamSizeResume(outSize));
-  return S_OK;
-}
+    HRESULT CDecoder::SetOutStreamSizeResume(const UInt64 * /*outSize*/)
+    {
+      _processedOut = 0;
+      return S_OK;
+    }
 
-HRESULT CDecoder::CodeSpec(ISequentialInStream * inStream,
-  ISequentialOutStream * outStream, ICompressProgressInfo * progress)
-{
-  LZ5MT_RdWr_t rdwr;
-  size_t result;
-  HRESULT res = S_OK;
+    STDMETHODIMP CDecoder::SetOutStreamSize(const UInt64 *outSize)
+    {
+      _processedIn = 0;
+      RINOK(SetOutStreamSizeResume(outSize));
+      return S_OK;
+    }
 
-  struct Lz5Stream Rd;
-  Rd.inStream = inStream;
-  Rd.processedIn = &_processedIn;
+    HRESULT CDecoder::CodeSpec(ISequentialInStream *inStream,
+                               ISequentialOutStream *outStream, ICompressProgressInfo *progress)
+    {
+      LZ5MT_RdWr_t rdwr;
+      size_t result;
+      HRESULT res = S_OK;
 
-  struct Lz5Stream Wr;
-  Wr.progress = progress;
-  Wr.outStream = outStream;
-  Wr.processedIn = &_processedIn;
-  Wr.processedOut = &_processedOut;
+      struct Lz5Stream Rd;
+      Rd.inStream = inStream;
+      Rd.processedIn = &_processedIn;
 
-  /* 1) setup read/write functions */
-  rdwr.fn_read = ::Lz5Read;
-  rdwr.fn_write = ::Lz5Write;
-  rdwr.arg_read = (void *)&Rd;
-  rdwr.arg_write = (void *)&Wr;
+      struct Lz5Stream Wr;
+      Wr.progress = progress;
+      Wr.outStream = outStream;
+      Wr.processedIn = &_processedIn;
+      Wr.processedOut = &_processedOut;
 
-  /* 2) create decompression context */
-  LZ5MT_DCtx *ctx = LZ5MT_createDCtx(_numThreads, _inputSize);
-  if (!ctx)
-      return S_FALSE;
+      /* 1) setup read/write functions */
+      rdwr.fn_read = ::Lz5Read;
+      rdwr.fn_write = ::Lz5Write;
+      rdwr.arg_read = (void *)&Rd;
+      rdwr.arg_write = (void *)&Wr;
 
-  /* 3) decompress */
-  result = LZ5MT_decompressDCtx(ctx, &rdwr);
-  if (LZ5MT_isError(result)) {
-    if (result == (size_t)-LZ5MT_error_canceled)
-      return E_ABORT;
-    return E_FAIL;
-  }
+      /* 2) create decompression context */
+      LZ5MT_DCtx *ctx = LZ5MT_createDCtx(_numThreads, _inputSize);
+      if (!ctx)
+        return S_FALSE;
 
-  /* 4) free resources */
-  LZ5MT_freeDCtx(ctx);
-  return res;
-}
+      /* 3) decompress */
+      result = LZ5MT_decompressDCtx(ctx, &rdwr);
+      if (LZ5MT_isError(result))
+      {
+        if (result == (size_t)-LZ5MT_error_canceled)
+          return E_ABORT;
+        return E_FAIL;
+      }
 
-STDMETHODIMP CDecoder::Code(ISequentialInStream * inStream, ISequentialOutStream * outStream,
-  const UInt64 * /*inSize */, const UInt64 *outSize, ICompressProgressInfo * progress)
-{
-  SetOutStreamSize(outSize);
-  return CodeSpec(inStream, outStream, progress);
-}
+      /* 4) free resources */
+      LZ5MT_freeDCtx(ctx);
+      return res;
+    }
+
+    STDMETHODIMP CDecoder::Code(ISequentialInStream *inStream, ISequentialOutStream *outStream,
+                                const UInt64 * /*inSize */, const UInt64 *outSize, ICompressProgressInfo *progress)
+    {
+      SetOutStreamSize(outSize);
+      return CodeSpec(inStream, outStream, progress);
+    }
 
 #ifndef NO_READ_FROM_CODER
-STDMETHODIMP CDecoder::SetInStream(ISequentialInStream * inStream)
-{
-  _inStream = inStream;
-  return S_OK;
-}
+    STDMETHODIMP CDecoder::SetInStream(ISequentialInStream *inStream)
+    {
+      _inStream = inStream;
+      return S_OK;
+    }
 
-STDMETHODIMP CDecoder::ReleaseInStream()
-{
-  _inStream.Release();
-  return S_OK;
-}
+    STDMETHODIMP CDecoder::ReleaseInStream()
+    {
+      _inStream.Release();
+      return S_OK;
+    }
 #endif
 
-HRESULT CDecoder::CodeResume(ISequentialOutStream * outStream, const UInt64 * outSize, ICompressProgressInfo * progress)
-{
-  RINOK(SetOutStreamSizeResume(outSize));
-  return CodeSpec(_inStream, outStream, progress);
-}
+    HRESULT CDecoder::CodeResume(ISequentialOutStream *outStream, const UInt64 *outSize, ICompressProgressInfo *progress)
+    {
+      RINOK(SetOutStreamSizeResume(outSize));
+      return CodeSpec(_inStream, outStream, progress);
+    }
 
-}}
+  }
+}

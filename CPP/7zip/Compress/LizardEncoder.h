b41f9c6c4c3c27b82d324dfa3dc08d3f28de964c
jinfeihan57 <jinfeihan57@gmail.com>
2022-05-24 20:40:32 +0800

    update 7zip compress code part6

--- CPP/7zip/Compress/LizardEncoder.h
+++ CPP/7zip/Compress/LizardEncoder.h
@@ -3,8 +3,8 @@
 #define LIZARD_STATIC_LINKING_ONLY
 #include "../../../C/Alloc.h"
 #include "../../../C/Threads.h"
-#include "../../../C/lizard/lib/lizard_compress.h"
-#include "../../../C/lizard/lib/lizard_frame.h"
+#include "../../../C/lizard/lizard_compress.h"
+#include "../../../C/lizard/lizard_frame.h"
 #include "../../../C/zstdmt/lizard-mt.h"
 
 #include "../../Common/Common.h"
@@ -13,63 +13,57 @@
 #include "../Common/StreamUtils.h"
 
 #ifndef EXTRACT_ONLY
-namespace NCompress
+namespace NCompress {
+namespace NLIZARD {
+
+struct CProps
 {
-  namespace NLIZARD
+  CProps() { clear (); }
+  void clear ()
   {
-
-    struct CProps
-    {
-      CProps() { clear(); }
-      void clear()
-      {
-        memset(this, 0, sizeof(*this));
-        _ver_major = LIZARD_VERSION_MAJOR;
-        _ver_minor = LIZARD_VERSION_MINOR;
-        _level = LIZARDMT_LEVEL_MIN;
-      }
-
-      Byte _ver_major;
-      Byte _ver_minor;
-      Byte _level;
-    };
-
-    class CEncoder : public ICompressCoder,
-                     public ICompressSetCoderMt,
-                     public ICompressSetCoderProperties,
-                     public ICompressWriteCoderProperties,
-                     public CMyUnknownImp
-    {
-      CProps _props;
-
-      UInt64 _processedIn;
-      UInt64 _processedOut;
-      UInt32 _inputSize;
-      UInt32 _numThreads;
-
-      LIZARDMT_CCtx *_ctx;
-
-    public:
-      MY_QUERYINTERFACE_BEGIN2(ICompressCoder)
-      MY_QUERYINTERFACE_ENTRY(ICompressSetCoderMt)
-      MY_QUERYINTERFACE_ENTRY(ICompressSetCoderProperties)
-      MY_QUERYINTERFACE_ENTRY(ICompressWriteCoderProperties)
-      MY_QUERYINTERFACE_END
-      MY_ADDREF_RELEASE
-
-      STDMETHOD(Code)
-      (ISequentialInStream *inStream, ISequentialOutStream *outStream, const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress);
-      STDMETHOD(SetCoderProperties)
-      (const PROPID *propIDs, const PROPVARIANT *props, UInt32 numProps);
-      STDMETHOD(WriteCoderProperties)
-      (ISequentialOutStream *outStream);
-      STDMETHOD(SetNumberOfThreads)
-      (UInt32 numThreads);
-
-      CEncoder();
-      virtual ~CEncoder();
-    };
-
+    memset(this, 0, sizeof (*this));
+    _ver_major = LIZARD_VERSION_MAJOR;
+    _ver_minor = LIZARD_VERSION_MINOR;
+    _level = LIZARDMT_LEVEL_MIN;
   }
-}
+
+  Byte _ver_major;
+  Byte _ver_minor;
+  Byte _level;
+};
+
+class CEncoder:
+  public ICompressCoder,
+  public ICompressSetCoderMt,
+  public ICompressSetCoderProperties,
+  public ICompressWriteCoderProperties,
+  public CMyUnknownImp
+{
+  CProps _props;
+
+  UInt64 _processedIn;
+  UInt64 _processedOut;
+  UInt32 _inputSize;
+  UInt32 _numThreads;
+
+  LIZARDMT_CCtx *_ctx;
+
+public:
+  MY_QUERYINTERFACE_BEGIN2(ICompressCoder)
+  MY_QUERYINTERFACE_ENTRY(ICompressSetCoderMt)
+  MY_QUERYINTERFACE_ENTRY(ICompressSetCoderProperties)
+  MY_QUERYINTERFACE_ENTRY(ICompressWriteCoderProperties)
+  MY_QUERYINTERFACE_END
+  MY_ADDREF_RELEASE
+
+  STDMETHOD (Code)(ISequentialInStream *inStream, ISequentialOutStream *outStream, const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress);
+  STDMETHOD (SetCoderProperties)(const PROPID *propIDs, const PROPVARIANT *props, UInt32 numProps);
+  STDMETHOD (WriteCoderProperties)(ISequentialOutStream *outStream);
+  STDMETHOD (SetNumberOfThreads)(UInt32 numThreads);
+
+  CEncoder();
+  virtual ~CEncoder();
+};
+
+}}
 #endif

0492790d2c6919c52e120fca055a06a030ce8c33
jinfeihan57 <jinfeihan57@gmail.com>
2022-05-24 20:39:03 +0800

    update 7zip compress code part 5

--- CPP/7zip/Compress/Lzma2Encoder.h
+++ CPP/7zip/Compress/Lzma2Encoder.h
@@ -4,10 +4,19 @@
 #define __LZMA2_ENCODER_H
 
 #include "../../../C/Lzma2Enc.h"
+#include "../../../C/fast-lzma2/fast-lzma2.h"
 
 #include "../../Common/MyCom.h"
 
 #include "../ICoder.h"
+
+#ifndef max
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
 
 namespace NCompress {
 namespace NLzma2 {
@@ -35,6 +44,56 @@ public:
 
   CEncoder();
   virtual ~CEncoder();
+};
+
+// fast lzma2 encode class`
+class CFastEncoder :
+  public ICompressCoder,
+  public ICompressSetCoderProperties,
+  public ICompressWriteCoderProperties,
+  public CMyUnknownImp
+{
+  class FastLzma2
+  {
+  public:
+    FastLzma2();
+    ~FastLzma2();
+    HRESULT SetCoderProperties(const PROPID *propIDs, const PROPVARIANT *props, UInt32 numProps);
+    size_t GetDictSize() const;
+    HRESULT Begin();
+    BYTE* GetAvailableBuffer(unsigned long& size);
+    HRESULT AddByteCount(size_t count, ISequentialOutStream *outStream, ICompressProgressInfo *progress);
+    HRESULT End(ISequentialOutStream *outStream, ICompressProgressInfo *progress);
+    void Cancel();
+
+  private:
+    bool UpdateProgress(ICompressProgressInfo *progress);
+    HRESULT WaitAndReport(size_t& res, ICompressProgressInfo *progress);
+    HRESULT WriteBuffers(ISequentialOutStream *outStream);
+
+    FL2_CStream* fcs;
+    FL2_dictBuffer dict;
+    size_t dict_pos;
+
+    FastLzma2(const FastLzma2&) = delete;
+    FastLzma2& operator=(const FastLzma2&) = delete;
+  };
+
+  FastLzma2 _encoder;
+
+public:
+  MY_UNKNOWN_IMP3(
+    ICompressCoder,
+    ICompressSetCoderProperties,
+    ICompressWriteCoderProperties)
+
+  STDMETHOD(Code)(ISequentialInStream *inStream, ISequentialOutStream *outStream,
+    const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress);
+  STDMETHOD(SetCoderProperties)(const PROPID *propIDs, const PROPVARIANT *props, UInt32 numProps);
+  STDMETHOD(WriteCoderProperties)(ISequentialOutStream *outStream);
+
+  CFastEncoder();
+  virtual ~CFastEncoder();
 };
 
 }}
